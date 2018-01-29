ee55d790586152f73b2acd777767271a6bc8edec
christian <csimon@wesim.de>
2018-01-29 20:54:54 +0100

    missing files

---
+++ src/main/java/de/wesim/imapnotes/SaveMessageTask.java
@@ -0,0 +1,22 @@
+package de.wesim.imapnotes;
+
+import javafx.concurrent.Task;
+import java.util.List;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+
+import javax.mail.Message;
+
+public class SaveMessageTask extends Task<ObservableList<Message>> {
+    private final IMAPBackend backend;
+
+    public SaveMessageTask( IMAPBackend backend) {
+        this.backend = backend;
+    }
+
+    @Override protected ObservableList<Message> call() throws Exception {
+        final List<Message> messages = this.backend.getMessages();	
+        return FXCollections.observableArrayList(messages);
+		
+    }
+}
