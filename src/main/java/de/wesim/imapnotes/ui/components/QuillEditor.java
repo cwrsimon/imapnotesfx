cd52c6a211ff3caedb5899e4385b3a1d5fc2c0ff
christian <csimon@wesim.de>
2018-04-18 22:11:56 +0200

    BugFix: QuillEditor

--- src/main/java/de/wesim/imapnotes/ui/components/QuillEditor.java
+++ src/main/java/de/wesim/imapnotes/ui/components/QuillEditor.java
@@ -1,6 +1,8 @@
 package de.wesim.imapnotes.ui.components;
 
 import org.apache.commons.text.StringEscapeUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javafx.scene.layout.StackPane;
 import javafx.scene.web.WebView;
@@ -8,6 +10,7 @@ import javafx.scene.web.WebView;
 public class QuillEditor extends StackPane {
   /** a webview used to encapsulate the CodeMirror JavaScript. */
   final WebView webview = new WebView();
+  private static final Logger log = LoggerFactory.getLogger(QuillEditor.class);
 
 
   public String getFullHTMLContent() {
@@ -32,6 +35,8 @@ public class QuillEditor extends StackPane {
   }
   
   public void setHtmlText(String content) {
+    log.info("Setting neu content: {}", content);
+    //final String newContent = "<div><b>Kündigungen</b></div><div>&nbsp;Thalia.de</div><div><br></div><div>R&amp;V : vor dem 1.5. 2017!</div><div>Huk Auslandskrankenversicherung demnächst!!! ?</div><div><br></div><div>Facebook (endgültig!)</div><div>Yahoo</div><div>Wesim -&gt; Alternative ?</div><p><ul><li>CWRSimon@gmail.com</li></ul></p><p><br></p><div>ticketonline</div><div>die zeit</div><div><br></div>";
 	  this.setHTMLContent(content);
   }
 
