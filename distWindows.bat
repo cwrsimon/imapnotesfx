824e97430318162721a13ee13529436f45f8dd49
Christian Simon <csimon@wesim.de>
2018-08-31 21:23:24 +0200

    erste Anfänge für eine Windows-Distro

---
+++ distWindows.bat
@@ -0,0 +1,2 @@
+mkdir -p dist
+C:\Program Files\Java\jdk-10.0.2\bin\javapackager -deploy -native image -srcdir target -srcfiles imapnotesfx-0.0.1-SNAPSHOT.jar  -name ImapNotesFX  -outdir dist -appclass MainApp
